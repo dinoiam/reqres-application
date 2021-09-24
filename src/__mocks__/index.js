7b65ef3d5ec6bc1d98d824133e8fd29cd56ee023
Michele Dinoia <michele.dinoia@musement.com>
2021-09-24 21:31:52 +0200

    test(mocks): add intersectionObserver mock

--- src/__mocks__/index.js
+++ src/__mocks__/index.js
@@ -3,3 +3,14 @@ global.localStorage = {
   getItem: jest.fn(),
   removeItem: jest.fn()
 };
+class IntersectionObserver {
+  observe = jest.fn();
+  disconnect = jest.fn();
+  unobserve = jest.fn();
+}
+
+Object.defineProperty(global, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserver
+});
